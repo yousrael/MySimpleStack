a7d24871ce2bd97a94811cadc942f1014bf95dea
Youyou <yousraelghzizal@hotmail.fr>
2016-02-19 14:20:32 +0100

    fichiers modifiés

--- src/main/java/dcll/yelg/Item.java
+++ src/main/java/dcll/yelg/Item.java
@@ -5,29 +5,6 @@ import java.util.EmptyStackException;
 /**
  * Created by HP on 19/02/2016.
  */
-public class Item implements SimpleStack {
-    @Override
-    public Item pop() throws EmptyStackException {
-        return null;
-    }
+public class Item {
 
-    @Override
-    public boolean isEmpty() {
-        return false;
-    }
-
-    @Override
-    public int getSize() {
-        return 0;
-    }
-
-    @Override
-    public void push(Item item) {
-
-    }
-
-    @Override
-    public Item peek() throws EmptyStackException {
-        return null;
-    }
 }
---
+++ src/main/java/dcll/yelg/MySimpleStack.java
@@ -0,0 +1,35 @@
+package dcll.yelg;
+
+import java.util.EmptyStackException;
+
+/**
+ * Created by HP on 19/02/2016.
+ */
+public class MySimpleStack implements SimpleStack {
+        @Override
+        public Item pop() throws EmptyStackException {
+            return null;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+
+        @Override
+        public int getSize() {
+            return 0;
+        }
+
+        @Override
+        public void push(Item item) {
+
+        }
+
+        @Override
+        public Item peek() throws EmptyStackException {
+            return null;
+        }
+    }
+
+
